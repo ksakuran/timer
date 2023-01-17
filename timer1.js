f1de75f59c93e2b96c02d8c4d3cf365be5c304da
Kat Nishimura <katherine.nishimura@live.ca>
2023-01-17 23:27:17 +0000

    alarm clock timer, that takes arguments from the command line, and beeps based on the arguments given

---
+++ timer1.js
@@ -0,0 +1,17 @@
+const args = process.argv.slice(2)
+const alarmClock = (array) => {
+  array.forEach(element => {
+    let num = Number(element);
+    if (Number.isNaN(num)) {
+      return;
+    }
+
+    if (num < 0) {
+      return;
+    }
+
+    setTimeout(()=>process.stdout.write('\x07'), num * 1000);
+   } );  
+}
+
+alarmClock(args);
